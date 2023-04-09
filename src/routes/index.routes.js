9e99b933585d454c5c708c3b2aaec0c8c408aff0
Leonel López <lalopez@cultura.gob.sv>
2023-04-09 17:10:06 -0600

    Fix status route

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -6,7 +6,7 @@ const {
 
 const route = Router();
 
-router.get("/status", (req, res) => {
+route.get("/status", (req, res) => {
   res.json({ message: "Active" });
 });
 route.get("/databases", getDatabases);
